19608e7dac09ac214b535833648c09fd605a38d7
Vigneshwaran <vigneshwaranmca22@gmail.com>
2024-12-17 21:00:19 +0530

    fixing the error

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('API is working!'));
+//app.get('/', (req, res) => res.send('API is working!'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
